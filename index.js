ab739f234082503a4bebbf29fd350fb2d65a6c25
Patricia Naomi <patricianaomiyama@gmail.com>
2023-07-05 23:45:09 -0300

    final exercício 1 da sala

---
+++ index.js
@@ -0,0 +1,13 @@
+//Exercício 1 - Botão
+function alteracao1() {
+    document.getElementById("ex1").innerHTML="Alteração no Texto <h3>1</h3>";
+    var obj1=document.getElementById("body1");
+    obj1.className="j1";
+}
+
+function alteracao2() {
+    document.getElementById("ex1").innerHTML="Alteração no Texto <h3>2</h3>";
+    var obj2=document.getElementById("body1");
+    obj2.className="j2";
+}
+ 
