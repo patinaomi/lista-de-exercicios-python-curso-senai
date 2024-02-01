2f964f989066392c3e3ebea97462fc65067fded5
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-01 10:18:10 -0300

    Update 007.py

--- mundo1/007.py
+++ mundo1/007.py
@@ -1,8 +1,11 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+valor = float(input('Digite o valor: '))
+taxa = float(input('Digite a taxa de juros: '))
+tempo = int(input('Digite a quantidade de meses: '))
 
-nota1 = float(input('Primeira nota do aluno: '))
-nota2 = float(input('Segunda nota do aluno: '))
+prestacao = valor + (valor * (taxa/100) * tempo)
 
-media = (nota1 + nota2) / 2
-
-print(f'A média entre {nota1} e {nota2} é igual a {media:.2f}')
+print('\n--- PRESTAÇÃO CONTAS ---')
+print(f'Valor em atraso R${valor:.2f}')
+print(f'Taxa de juros: {taxa:.1f}%')
+print(f'Quantidade de meses em atraso: {tempo}')
+print(f'Valor atualizado: R${prestacao:.2f}')
