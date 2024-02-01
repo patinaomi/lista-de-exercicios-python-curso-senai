5b678dec52dc97c50404522b4c630b8ed9a32fbf
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-01 10:17:42 -0300

    Update 005.py

--- mundo1/005.py
+++ mundo1/005.py
@@ -1,5 +1,2 @@
-# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
-
 numero = int(input('Digite um número: '))
-
-print(f'Analisando o valor {numero}, seu antecessor é {numero - 1} e o seu sucessor é {numero + 1}.')
+print(f'Número digitado {numero}\nAntecessor: {numero-1}\nSucessor: {numero+1}')
