8409ce78ee6a49857122873b46507bfcdd00669f
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-01 10:12:36 -0300

    Update 003.py

--- mundo1/003.py
+++ mundo1/003.py
@@ -1,8 +1,2 @@
-# Crie um programa que leia dois números e mostre a soma entre eles.
-
-n1 = int(input('Digite o valor 1: '))
-n2 = int(input('Digite o valor 2: '))
-
-soma = n1 + n2
-
-print(f'Resultado: {n1} + {n2} = {soma}')
+nome = input('Digite seu nome: )
+print(f'{nome}, boas vindas!)
