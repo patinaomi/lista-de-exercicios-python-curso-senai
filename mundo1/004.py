ad1e609c0b63ae98c28a38422b257942552478c6
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-01 10:17:24 -0300

    Update 004.py

--- mundo1/004.py
+++ mundo1/004.py
@@ -1,12 +1,4 @@
-# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+caracter1 = input('Digite o primeiro caracter: ')
+caracter2 = input('Digite o segundo caracter: ')
 
-variavel = input('Digite algo: ')
-
-print(f'O tipo primitivo desse valor é: {type(variavel)}')
-print(f'Só tem espaços? {variavel.isspace()}')
-print(f'É um número?  {variavel.isnumeric()}')
-print(f'É alfabético?  {variavel.isalpha()}')
-print(f'É alfanumérico? {variavel.isalnum()}')
-print(f'Está em maiúsculas? {variavel.isupper()}')
-print(f'Está em minúsculas? {variavel.islower()}')
-print(f'Está em capitalizada? {variavel.istitle()}')
+print(f'O usuário digitou o {caracter1} e {caracter2}.')
