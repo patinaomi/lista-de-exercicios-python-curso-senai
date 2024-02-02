913641dc004367be530936b7977dcabbafd260db
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-02 09:01:38 -0300

    Create 016.py

---
+++ semana1/016.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 == num2:
+  print('Os números digitados são iguais')
+else:
+  print('Os números digitados são diferentes')
