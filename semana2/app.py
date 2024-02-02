5aff1800687b2c7100d67f566e9ea9e6ddafe7e8
Patricia Naomi <patricianaomiyama@gmail.com>
2024-02-02 16:45:18 -0300

    alterações pasta

---
+++ semana2/app.py
@@ -0,0 +1,14 @@
+print('🅢🅐🅑🅞🅡 🅔🅧🅟🅡🅔🅢🅢')
+
+print("""1. Cadastrar restaurante
+2. Listar restaurante
+3. Ativar restaurante
+4. Sair\n""")
+
+# opcao = input('Digite uma opção: ')
+
+nome = 'A sua média é '
+
+pi = 3.133103109
+
+print(round(pi, 4))
