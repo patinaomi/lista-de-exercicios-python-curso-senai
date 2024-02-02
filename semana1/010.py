eb782f5ea08225d35c428b9e6bad83342d16c6ae
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-02 08:59:59 -0300

    Update 010.py

--- semana1/010.py
+++ semana1/010.py
@@ -1,9 +1,6 @@
-n1 = float(input('Digite a primeira nota do aluno: '))
-n2 = float(input('Digite a segunda nota do aluno: '))
+salario = float(input('Digite o valor do salário atual: '))
+percentual = float(input('Digite o percentual de aumento: '))
 
-media = (n1 + n2) / 2
+salario_atualizado = salario + (salario * (percentual/100))
 
-if media >= 6:
-  print('Aprovado')
-else:
-  print('Reprovado')
+print(f'Saldo atualizado: R${salario_atualizado:.2f}')
