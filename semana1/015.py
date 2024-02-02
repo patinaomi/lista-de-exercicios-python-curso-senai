77ab3984246c5d382458f530a0bff5a232b263f2
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-02 09:01:20 -0300

    Update 015.py

--- semana1/015.py
+++ semana1/015.py
@@ -1,8 +1,8 @@
-# Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por Km rodado. 
+ano_atual = 2024
 
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos km rodados? '))
+ano_nascimento = int(input('Digite seu ANO de nascimento: '))
 
-preco_total = (60 * dias) + (0.15 * km)
-
-print(f'O total a pagar é de R${preco_total:.2f}')
+if ano_atual - ano_nascimento >= 18:
+  print('Você é MAIOR de idade')
+else:
+  print('Você é MENOR de idade')
