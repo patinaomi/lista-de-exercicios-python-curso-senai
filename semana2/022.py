5276dbf6d3c4bfebfa4cb96cb5bec782e5921861
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-07 10:04:47 -0300

    Create 022.py

---
+++ semana2/022.py
@@ -0,0 +1,10 @@
+# exemplo 1
+print('EXEMPLO 1: ')
+for i in range(1, 101):
+  if i %2 == 0:
+    print(i, end=',')
+
+# exemplo 2
+print('\nEXEMPLO 2: ')
+for i in range (2, 101, 2):
+  print(i, end = ',')
