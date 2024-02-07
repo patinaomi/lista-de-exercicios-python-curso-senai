04b24891642246fc11d1cb20d0642863b17ee64b
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-07 11:21:08 -0300

    Update 024.py

--- semana2/024.py
+++ semana2/024.py
@@ -1,30 +1,26 @@
 lista = []
 soma = 0
 cont = 0
-cont2 = 0
 
 qtd = int(input('Informe a quantidade de clientes: '))
 
 for i in range(qtd):
     lista.append(float(input(f'Informe a temperatura do {i+1}° cliente: ')))
     soma += lista[i]
-    cont += 1
 
 print('\n--- RESULTADO ---') 
 for item in lista:
-    cont2 += 1
+    cont += 1
     if item <= 37.2:
-        print(f'Paciente {cont2} : {item}°C - Temperatura Normal')
+        print(f'Paciente {cont} : {item}°C - Temperatura Normal')
     elif item <= 38:
-        print(f'Paciente {cont2} : {item}°C - Estado Febril')
+        print(f'Paciente {cont} : {item}°C - Estado Febril')
     elif item <= 39:
-        print(f'Paciente {cont2} : {item}°C - Febre')
+        print(f'Paciente {cont} : {item}°C - Febre')
     else:
-        print(f'Paciente {cont2} : {item}°C - Febre Alta')
+        print(f'Paciente {cont} : {item}°C - Febre Alta')
 
 
 print('\n---- RELATÓRIO ----')
-print(f'Quantidade de pessoas analisadas: {cont}')
+print(f'Quantidade de pessoas analisadas: {qtd}')
 print(f'Média da temperatura: {soma/cont:.2f}°C')
-
-
