8ca721204eac17591a663d6b175d389454689bfd
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-08 11:32:08 -0300

    Create 029.py

---
+++ semana2/029.py
@@ -0,0 +1,15 @@
+usuario = 'patinaomi'
+senha = 'goiaba'
+
+while True:
+    usuario_login = input('Usuário: ')
+    senha_login = input('Senha: ')
+
+    if usuario_login == senha_login:
+        print('A senha não pode ser igual ao nome do usuário')
+        continue
+    if usuario_login == usuario and senha_login == senha:
+        print('Login realizado com sucesso!')
+        break
+    else:
+        print('Não foi possível realizar o login')
