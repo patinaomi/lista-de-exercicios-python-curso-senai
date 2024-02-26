3fbbefd5e9c63e7a3deeff9c580d8602271680e3
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-02-26 11:31:13 -0300

    Create 031.py

---
+++ semana 3/031.py
@@ -0,0 +1,43 @@
+# Sistema para buscar determinado paciente da lista
+
+lista_pacientes = []
+id = 0
+
+while True:
+    print('--- MENU ---')
+    print('[1] Cadastrar Paciente')
+    print('[2] Buscar Paciente')
+    print('[0] Sair do Programa')
+    opcao = input('Digite uma opção: ')
+
+    if opcao == '1':
+        while True:
+            print('----- CADASTRAR NOVO PACIENTE -----')
+            nome = input('Digite o nome: ')
+            sobrenome = input('Digite o sobrenome: ')
+            idade = int(input('Digite a idade: '))
+            id += 1
+            paciente = {'Id': id, 'Nome': nome, 'Sobrenome': sobrenome, 'Idade': idade}
+            lista_pacientes.append(paciente)
+
+            opcao = input('Deseja adicionar novo paciente? (S/N): ')
+            if opcao.upper() == 'N':
+                break
+
+    elif opcao == '2':
+            print('----- BUSCAR PACIENTE -----')
+            id_busca = int(input('Digite o Id do paciente: '))
+            for k in paciente:
+                if paciente[k] == id_busca:
+                    print('Usuário encontrado: ')
+                    print(paciente)
+                    break
+            else:
+                print('Paciente não encontrado na lista')
+
+    elif opcao == '0':
+        print('Saindo do Programa...')
+        break
+
+    else:
+        print('Opção Inválida')
