ef252f293a0cab53d86f0840201ea3ab3cb9e645
Patricia Naomi <patricianaomiyama@gmail.com>
2024-03-08 01:10:26 -0300

    finalização código

--- semana4/projeto.py
+++ semana4/projeto.py
@@ -2,6 +2,7 @@ import json
 
 
 def menu_principal():
+    # Pra criar um loop infinito
     while True:
         print("""
 ------ Loja da Pati ------
@@ -11,76 +12,109 @@ def menu_principal():
 | [4] Buscar Produto    |
 | [5] Deletar Produto   |
 | [0] Sair do Programa  |
--------------------------
-""")
+-------------------------""")
+        try:
+            op = int(input('Digite uma opção: '))
 
-        op = int(input('Digite uma opção: '))
+            if op in range(0, 6):
+                if op == 1:
+                    adicionar_prod()
+                elif op == 2:
+                    editar_prod()
+                elif op == 3:
+                    listar_prod()
+                elif op == 4:
+                    buscar_prod()
+                elif op == 5:
+                    deletar_prod()
+                elif op == 0:
+                    print('Encerrando o programa...')
+                    break
+            else:
+                print('Opção inválida, digite novamente')
 
-        if op in range(0, 6):
-            if op == 1:
-                adicionar_prod()
-            elif op == 2:
-                editar_prod()
-            elif op == 3:
-                listar_prod()
-            elif op == 4:
-                buscar_prod()
-            elif op == 5:
-                deletar_prod()
-            elif op == 0:
-                print('Encerrando o programa...')
-                break
-        else:
-            print('Opção inválida, digite novamente')
+        # Se o usuário digitar algo que não seja números
+        except ValueError:
+            print('Digite somente números')
 
 
 #
 def carregar_dados():
-    with open('estoque.json', 'r', encoding='utf-8') as arquivo:
-        dados = json.load(arquivo)
+    try:
+        with open('estoque.json', 'r', encoding='utf-8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Não foi encontrado o arquivo de estoque')
+        return
+
     return dados
 
 
 #
 def escrever_dados(dados):
-    with open('estoque.json', 'w', encoding='utf-8') as arquivo:
-        json.dump(dados, arquivo, indent=2, ensure_ascii=False)
+    try:
+        with open('estoque.json', 'w', encoding='utf-8') as arquivo:
+                                # pra acentuação etc, ficar correta
+            json.dump(dados, arquivo, indent=2, ensure_ascii=False)
+    except FileNotFoundError:
+        print('Não foi encontrado o arquivo de estoque')
+        return
 
 
-#
 def imprimir_dados(dados):
     print(f"\n----- ID #{dados['Id']} -----")
     print(f"Produto: {dados['Produto']}")
     print(f"Cor: {dados['Cor']}")
     print(f"Preço: R${dados['Preço']} un")
-    variacoes = dados["Variações"]
+    variacoes = dados['Variações']
+    # pra poder percorrer todos os itens que vão estar no dic de variações
     for tamanho, quantidade in variacoes.items():
         print(f"Tam: {tamanho} - {quantidade} pçs")
 
 
-#
 def adicionar_prod():
     lista_tam = {}
     while True:
         print('------ Adicionar Produto ------')
         produto = input('Digite o nome do Produto: ')
         cor = input('Digite a cor do Produto: ')
-        preco = float(input('Digite o preço unitário: '))
-        qtd_var = int(input('Digite a quantidade de variações: '))
+
+        while True:
+            preco = float(input('Digite o preço unitário: '))
+            if preco < 0:
+                print('Digite um valor válido')
+            else:
+                break
+
+        while True:
+            qtd_var = int(input('Digite a quantidade de variações: '))
+            if qtd_var < 0:
+                print('Digite um valor válido')
+            else:
+                break
 
         for i in range(qtd_var):
             variacao = input(f'Variação {i + 1}: ')
             qtd = input('Digite a quantidade: ')
             lista_tam.update({variacao.upper(): qtd})
 
-        dados = carregar_dados()
-        id = len(dados) + 1
+        produtos = carregar_dados()
+
+        id_em_uso = True
+        while id_em_uso:
+            id_em_uso = False
+            id = int(input('Digite o Id do produto: '))
+            for produto_existente in produtos:
+                if produto_existente['Id'] == id:
+                    print("ID já está em uso. Por favor, digite um ID diferente.")
+                    id_em_uso = True
+                    break
 
         novo_prod = {'Id': id, 'Produto': produto.capitalize(), 'Cor': cor.capitalize(), 'Preço': preco,
                      'Variações': lista_tam}
 
-        dados.append(novo_prod)
-        escrever_dados(dados)
+        produtos.append(novo_prod)
+        escrever_dados(produtos)
 
         print(f'Produto {produto} cadastrado com sucesso!')
         break
@@ -133,9 +167,14 @@ def editar_prod():
                     return
                 else:
                     print('Opção inválida')
+                    break
 
                 escrever_dados(produtos)
                 print('Produto alterado com sucesso')
+                break
+
+            else:
+                print('Id não encontrado')
                 break
 
     except ValueError:
@@ -196,12 +235,16 @@ def deletar_prod():
             if produto['Id'] == busca:
                 index = i
                 del produtos[index]
+                print('Produto deletado')
+                escrever_dados(produtos)
+                break
 
-        escrever_dados(produtos)
-        print('Produto deletado')
+        else:
+            print('Produto não foi encontrado')
 
     except ValueError:
         print('Digite somente números')
 
 
+# Início do Programa
 menu_principal()
