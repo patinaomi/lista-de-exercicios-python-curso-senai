2f4b009a15d6c90c1a0fbb025c2bd109975e7e0f
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-11 08:37:12 -0300

    Update 029.py

--- semana2/029.py
+++ semana2/029.py
@@ -5,10 +5,10 @@ while True:
     usuario_login = input('Usuário: ')
     senha_login = input('Senha: ')
 
-    if usuario_login == senha_login:
+    if usuario_login.lower() == senha_login:
         print('A senha não pode ser igual ao nome do usuário')
         continue
-    if usuario_login == usuario and senha_login == senha:
+    if usuario_login.lower() == usuario and senha_login == senha:
         print('Login realizado com sucesso!')
         break
     else:
