9658f670da8a7df9b669516aa2f0084d1e2ea82f
Patricia Naomi <132932532+patinaomi@users.noreply.github.com>
2024-03-11 08:45:57 -0300

    Update 031.py

--- semana3/031.py
+++ semana3/031.py
@@ -4,40 +4,45 @@ lista_pacientes = []
 id = 0
 
 while True:
-    print('--- MENU ---')
+    print('----- MENU -----')
     print('[1] Cadastrar Paciente')
     print('[2] Buscar Paciente')
     print('[0] Sair do Programa')
-    opcao = input('Digite uma opção: ')
 
-    if opcao == '1':
-        while True:
-            print('----- CADASTRAR NOVO PACIENTE -----')
-            nome = input('Digite o nome: ')
-            sobrenome = input('Digite o sobrenome: ')
-            idade = int(input('Digite a idade: '))
-            id += 1
-            paciente = {'Id': id, 'Nome': nome, 'Sobrenome': sobrenome, 'Idade': idade}
+    try:
+        opcao = int(input('Digite uma opção: '))
+        if opcao in range(0, 3):
+            if opcao == 1:
+                while True:
+                    print('----- CADASTRAR NOVO PACIENTE -----')
+                    nome = input('Digite o nome: ')
+                    sobrenome = input('Digite o sobrenome: ')
+                    idade = int(input('Digite a idade: '))
+                    id += 1
+                    paciente = {'Id': id, 'Nome': nome, 'Sobrenome': sobrenome, 'Idade': idade}
 
-            print('Paciente cadastrado com sucesso')
-            lista_pacientes.append(paciente)
+                    print('Paciente cadastrado com sucesso')
+                    lista_pacientes.append(paciente)
 
-            break
-
-    elif opcao == '2':
-            print('----- BUSCAR PACIENTE -----')
-            id_busca = int(input('Digite o Id do paciente: '))
-            for k in paciente:
-                if paciente[k] == id_busca:
-                    print('Usuário encontrado: ')
-                    print(paciente)
                     break
-            else:
-                print('Paciente não encontrado na lista')
 
-    elif opcao == '0':
-        print('Saindo do Programa...')
-        break
+            elif opcao == 2:
+                    print('----- BUSCAR PACIENTE -----')
+                    id_busca = int(input('Digite o Id do paciente: '))
+                    for k in paciente:
+                        if paciente[k] == id_busca:
+                            print('Usuário encontrado: ')
+                            print(paciente)
+                            break
+                    else:
+                        print('Paciente não encontrado na lista')
 
-    else:
-        print('Opção Inválida')
+            elif opcao == 0:
+                print('Saindo do Programa...')
+                break
+
+        else:
+            print('Opção Inválida')
+            
+    except ValueError:
+        print('Digite somente números')
